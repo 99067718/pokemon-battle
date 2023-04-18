3a69526f5944d16ffa8234de536e616133032499
Mielesgames <emieltlam11@gmail.com>
2023-04-18 11:57:57 +0200

    this stupid program isn't working and does not give a proper explanation#

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using pokemon.Models;
+Characters characters = new Characters();
+Console.WriteLine(characters.Pikachu.ChosenName);
+var e = Console.ReadLine();
+Console.WriteLine("Hello, World!" + e);
